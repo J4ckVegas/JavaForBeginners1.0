c56d1c8c0be29ac2e157e64755438cce9b200697
JackVegas <sickjackvegas@gmail.com>
2019-02-11 15:31:11 +0300

    Initial commit

---
+++ src/Task5.java
@@ -0,0 +1,18 @@
+package com.company;
+/*
+Ввести с консоли число в бинарном формате.
+Перевести его в int и вывести на экран.
+*/
+import java.util.Scanner;
+
+public class Task5
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число в бинарном формате: ");
+        String binary = scan.next();
+        int Y = Integer.parseInt(binary, 2);
+        System.out.println("Результат: " + Y);
+    }
+}
