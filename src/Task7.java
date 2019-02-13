5deba0ca657b455c9291fa5952e4354d6bd318a9
JackVegas <sickjackvegas@gmail.com>
2019-02-13 14:00:09 +0300

    Задача №7

---
+++ src/Task7.java
@@ -0,0 +1,8 @@
+/*Необходимо с помощью цикла вывести на экран числа от 1 до 50*/
+public class Task7 {
+        public static void main(String args[]){
+            for (int j = 1; j <= 50; j++) {
+                System.out.print (j + " ");
+            }
+        }
+    }
