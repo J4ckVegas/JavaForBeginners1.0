6545876e8b663bd90fa5bb06e9957b1e054dcf2e
JackVegas <sickjackvegas@gmail.com>
2019-02-13 10:55:40 +0300

    Заливаю весь проект. Вношу косметические правки в ранее сделанные задачи.

--- src/Task5.java
+++ src/Task5.java
@@ -1,14 +1,8 @@
-package com.company;
-/*
-Ввести с консоли число в бинарном формате.
-Перевести его в int и вывести на экран.
-*/
+/* Ввести с консоли число в бинарном формате.
+Перевести его в int и вывести на экран. */
 import java.util.Scanner;
-
-public class Task5
-{
-    public static void main(String args[])
-    {
+public class Task5 {
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите число в бинарном формате: ");
         String binary = scan.next();
