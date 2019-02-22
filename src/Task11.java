31fb9d2e9b95e719c3fc59021ecf94487c6bafbf
JackVegas <sickjackvegas@gmail.com>
2019-02-22 10:13:37 +0300

    Task11 completed

---
+++ src/Task11.java
@@ -0,0 +1,11 @@
+/*Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.*/
+import java.util.Scanner;
+public class Task11 {
+    public static void  main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String X = scan.nextLine();
+        System.out.println("Введенный Вами текст: " + X);
+
+    }
+}
