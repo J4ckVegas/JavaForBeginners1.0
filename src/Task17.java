8b136d697fb422e98547c7277d5edaf9c6b1c45d
J4ckVegas <sickjackvegas@gmail.com>
2019-03-08 19:55:11 +0300

    Task17 completed

---
+++ src/Task17.java
@@ -0,0 +1,24 @@
+/*Необходимо написать программу, которая будет записывать текстовые данные, введенные с экрана, в файл .txt.
+Запись ввести в файл до тех пор, пока не будет введена команда стоп в консоли.
+Пользователь при тестировании программы по правилу черного ящика должен понимать, какой принцип остановки записи в файл он должен подать.*/
+import java.io.*;
+
+public class Task17 {
+
+    public static void main(String[] args) {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Признак конца ввода - строка 'stop' ");
+        try (FileWriter nFile = new FileWriter("sample2.txt")) {
+            do {
+                System.out.print(": ");
+                str = br.readLine();
+                if (str.compareTo("stop") == 0) break;
+                str = str + "\r\n";
+                nFile.write(str);
+            } while (str.compareTo("stop") != 0);
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода/вывода: " + exc);
+        }
+    }
+}
