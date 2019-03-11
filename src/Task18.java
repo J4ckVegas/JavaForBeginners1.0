2c2439ffcd9cc0b0ad0815f6d73ccd7647ee3330
J4ckVegas <sickjackvegas@gmail.com>
2019-03-11 19:02:34 +0300

    Task18 правки

--- src/Task18.java
+++ src/Task18.java
@@ -5,45 +5,25 @@
 import java.io.*;
 
 public class Task18 {
-
     public static void main(String[] args) {
         String s;
         int i, j;
         i = j = 0;
-
         try(BufferedReader br = new BufferedReader(new FileReader("sample3.txt"))) {
-
             while((s = br.readLine())!= null){
                 System.out.print(s + "\n");
-
                 i += 1;
             }
             System.out.println("Кол-во строк: " + i);
-
             String str;
             BufferedReader brd = new BufferedReader (new InputStreamReader(System.in));
-            System.out.println("Для обработки строки - введите команду 'stop'");
-
             try(FileWriter fw = new FileWriter("sample3.txt", false)) {
-                do {
+                for (int k = 0; k < i; k++) {
                     System.out.print("Введите текст: ");
                     str = brd.readLine();
-
-                    if(str.compareTo("stop") == 0) break;
-
                     str += "\r\n";
                     fw.write(str);
-
-                    j += 1;
-
-                    if (j > i) {
-                        System.out.print("Ошибка! В последующем тексте количество строк больше чем в изначальном");
-                        break;
-                    }
                 }
-                while (str.compareTo("stop")!= 0);
-                if (i > j){
-                    System.out.print("Ошибка! В последующем тексте количество строк меньше чем в изначальном"); }
             }
             catch(IOException ex){
                 System.out.println(ex.getMessage()); }
