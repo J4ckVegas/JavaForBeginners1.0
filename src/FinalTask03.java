8ba5cc07d784648f2a3dcba9bbf5b3c407214086
J4ckVegas <sickjackvegas@gmail.com>
2019-03-17 22:14:56 +0300

    FinalTask03 completed

---
+++ src/FinalTask03.java
@@ -0,0 +1,15 @@
+/*Напишите программу конвертер валют. Программа должна переводить рубли в доллары по текущему курсу.
+Пользователь вводит текущий курс и количество рублей.*/
+
+import java.util.Scanner;
+public class FinalTask03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите текущий курс доллара: ");
+        double usd = scan.nextDouble();
+        System.out.print("Введите количество рублей для конвертации: ");
+        double rur = scan.nextDouble();
+        double res = (rur/usd);
+        System.out.println(rur + "рублей =" + res + "долларов");
+    }
+}
